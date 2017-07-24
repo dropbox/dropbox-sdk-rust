4848dc171f2fa94afd3769ff52cc37db1653f5a7
Bill Fraser <wfraser@dropbox.com>
2017-07-24 12:10:22 -0700

    add the hyper client and helpers from the dbxfs codebase

--- src/client_helpers.rs
+++ src/client_helpers.rs
@@ -1,1 +1,66 @@
-// TODO
+use std::io::Read;
+use ResultExt;
+use client_trait::*;
+use serde::de::DeserializeOwned;
+use serde::ser::Serialize;
+use serde_json;
+
+pub struct HttpRequestResult<T> {
+    pub result: T,
+    pub content_length: Option<u64>,
+    pub body: Option<Box<Read>>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TopLevelError<T> {
+    pub error_summary: String,
+    pub user_message: Option<String>,
+    pub error: T,
+}
+
+/// Does the request and returns a two-level result. The outer result has an error if something
+/// went horribly wrong (I/O errors, parse errors, server 500 errors, etc.). The inner result has
+/// an error if the server returned one for the request, otherwise it has the deserialized JSON
+/// response and the body stream (if any).
+pub fn request_with_body<T: DeserializeOwned, E: DeserializeOwned, P: Serialize>(
+    client: &HttpClient,
+    endpoint: Endpoint,
+    function: &str,
+    params: &P,
+    body: Option<Vec<u8>>,
+    range_start: Option<u64>,
+    range_end: Option<u64>,
+) -> super::Result<Result<HttpRequestResult<T>, E>> {
+    let params_json = serde_json::to_string(params)?;
+    let result = client.request(endpoint, function, params_json, body, range_start, range_end);
+    match result {
+        Ok(HttpRequestResultRaw { result_json, content_length, body }) => {
+            debug!("json: {}", result_json);
+            let result_value: T = serde_json::from_str(&result_json)?;
+            Ok(Ok(HttpRequestResult {
+                result: result_value,
+                content_length,
+                body,
+            }))
+        },
+        Err(super::Error(super::ErrorKind::ApiFailure(ref code, ref _status, ref json), _)) if *code == 409 => {
+            let err = serde_json::from_str::<TopLevelError<E>>(json)?;
+            Ok(Err(err.error))
+        },
+        Err(e) => {
+            error!("{}", e);
+            Err(e).chain_err(|| super::ErrorKind::ApiError("API returned garbage"))
+        }
+    }
+}
+
+pub fn request<T: DeserializeOwned, E: DeserializeOwned, P: Serialize>(
+    client: &HttpClient,
+    endpoint: Endpoint,
+    function: &str,
+    params: &P,
+    body: Option<Vec<u8>>,
+) -> super::Result<Result<T, E>> {
+    request_with_body(client, endpoint, function, params, body, None, None)
+        .map(|result| result.map(|HttpRequestResult { result, .. }| result))
+}
--- src/hyper_client.rs
+++ src/hyper_client.rs
@@ -1,8 +1,34 @@
+#![allow(unknown_lints)]
+
+use std::io::{self, Read};
+use std::str;
+
 use ::ErrorKind;
 use client_trait::{Endpoint, HttpClient, HttpRequestResultRaw};
+use hyper::{self, Url};
+use hyper::header::*;
+use hyper_native_tls;
+
+const USER_AGENT: &'static str = "Dropbox-APIv2-Rust/0.1";
 
 pub struct HyperClient {
-    // TODO
+    client: hyper::client::Client,
+    token: String,
+}
+
+impl HyperClient {
+    pub fn new(token: String) -> HyperClient {
+        let tls = hyper_native_tls::NativeTlsClient::new().unwrap();
+        let https_connector = hyper::net::HttpsConnector::new(tls);
+        let pool_connector = hyper::client::pool::Pool::with_connector(
+            hyper::client::pool::Config { max_idle: 1 },
+            https_connector);
+        let client = hyper::client::Client::with_connector(pool_connector);
+        HyperClient {
+            client,
+            token,
+        }
+    }
 }
 
 impl HttpClient for HyperClient {
@@ -14,7 +40,105 @@ impl HttpClient for HyperClient {
         body: Option<Vec<u8>>,
         range_start: Option<u64>,
         range_end: Option<u64>,
-    ) -> ::Result<HttpRequestResultRaw> {
-        unimplemented!()
+    ) -> super::Result<HttpRequestResultRaw> {
+
+        let url = Url::parse(endpoint.url()).unwrap().join(function).expect("invalid request URL");
+        debug!("request for {:?}", url);
+
+        #[allow(never_loop)] // this is a false positive
+        loop {
+            let mut builder = self.client.post(url.clone());
+
+            let mut headers = Headers::new();
+            headers.set(UserAgent(USER_AGENT.to_owned()));
+            headers.set(Authorization(Bearer { token: self.token.clone() }));
+            headers.set(Connection::keep_alive());
+
+            if let Some(start) = range_start {
+                if let Some(end) = range_end {
+                    headers.set(Range::Bytes(vec![ByteRangeSpec::FromTo(start, end)]));
+                } else {
+                    headers.set(Range::Bytes(vec![ByteRangeSpec::AllFrom(start)]));
+                }
+            } else if let Some(end) = range_end {
+                headers.set(Range::Bytes(vec![ByteRangeSpec::Last(end)]));
+            }
+
+            // If the params are totally empt, don't send any arg header or body.
+            if !params_json.is_empty() {
+                match endpoint {
+                    Endpoint::Api => {
+                        // Send params in the body.
+                        headers.set(ContentType::json());
+                        builder = builder.body(params_json.as_bytes());
+                        assert_eq!(None, body);
+                    },
+                    Endpoint::Content => {
+                        // Send params in a header.
+                        headers.set_raw("Dropbox-API-Arg", vec![params_json.clone().into_bytes()]);
+                        if let Some(body) = body.as_ref() {
+                            builder = builder.body(body.as_slice());
+                        }
+                    }
+                }
+            }
+
+            let mut resp = match builder.headers(headers).send() {
+                Ok(resp) => resp,
+                Err(hyper::error::Error::Io(ref ioerr))
+                        if ioerr.kind() == io::ErrorKind::ConnectionAborted => {
+                    debug!("connection closed; retrying...");
+                    continue;
+                },
+                Err(other) => {
+                    error!("request failed: {}", other);
+                    return Err(other.into());
+                }
+            };
+
+            if !resp.status.is_success() {
+                let (code, status) = {
+                    let &hyper::http::RawStatus(ref code, ref status) = resp.status_raw();
+                    use std::ops::Deref;
+                    (*code, status.deref().to_owned())
+                };
+                let mut json = String::new();
+                resp.read_to_string(&mut json)?;
+                return Err(ErrorKind::ApiFailure(code, status, json).into());
+            }
+
+            return match endpoint {
+                Endpoint::Api => {
+                    // Get the response from the body; return no body stream.
+                    let mut s = String::new();
+                    resp.read_to_string(&mut s)?;
+                    Ok(HttpRequestResultRaw {
+                        result_json: s,
+                        content_length: None,
+                        body: None,
+                    })
+                },
+                Endpoint::Content => {
+                    // Get the response from a header; return the body stream.
+                    let s = match resp.headers.get_raw("Dropbox-API-Result") {
+                        Some(values) => {
+                            String::from_utf8(values[0].clone())?
+                        },
+                        None => {
+                            return Err(ErrorKind::ApiError("missing Dropbox-API-Result header").into());
+                        }
+                    };
+
+                    let len = resp.headers.get::<ContentLength>().map(|h| h.0);
+
+                    Ok(HttpRequestResultRaw {
+                        result_json: s,
+                        content_length: len,
+                        body: Some(Box::new(resp)),
+                    })
+                }
+            }
+
+        }
     }
 }
