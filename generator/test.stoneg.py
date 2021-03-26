dc05148d460a08efaeced3492877e68b9f67573a
Bill Fraser <wfraser@codewise.org>
2021-03-25 21:49:07 -0700

    allow clippy::collapsible-match in tests
    This is a new lint, and changing the test codegen to not run afoul of it would be pretty hard, and for test code it doesn't matter enough.

--- generator/test.stoneg.py
+++ generator/test.stoneg.py
@@ -92,6 +92,7 @@ class TestBackend(RustHelperBackend):
         self.emit(u'    clippy::float_cmp,')
         self.emit(u'    clippy::unreadable_literal,')
         self.emit(u'    clippy::cognitive_complexity,')
+        self.emit(u'    clippy::collapsible_match,')
         self.emit(u')]')
         self.emit()
 
